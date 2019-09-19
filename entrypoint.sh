9a582789d487c54ab771818674bd1760c95319a3
Dmitry Shmelev <dmitry.shmelev@jetbrains.com>
2019-09-19 17:50:41 +0300

    Fix envsubst

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # default behaviour is to launch nginx
-envsubst < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
+envsubst '\$S3_BUCKET_DOMAIN_NAME \$S3_BUCKET' < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
 
 echo "Starting nginx..."
 exec nginx -g "daemon off;"
