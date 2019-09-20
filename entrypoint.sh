c8c0afb3f77cc9474a3c73c0fcaa6032a59dfd89
Dmitry Shmelev <dmitry.shmelev@jetbrains.com>
2019-09-20 12:17:49 +0300

    Fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 envsubst '\$S3_BUCKET_DOMAIN_NAME \$S3_BUCKET' < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
 
 echo "Starting nginx..."
-exec nginx -g "daemon off;"
+exec /usr/local/nginx/sbin/nginx -g "daemon off;"
